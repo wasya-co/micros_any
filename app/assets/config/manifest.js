5a00305d98e722b61c746e447edc224b9c41ed95
mac_a2141 <victor@piousbox.com>
2023-12-16 15:56:52 -0500

    hmm

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link wco_email/application.css
+//= link wco_hosting/application.css
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'application#home'
 
 
-  mount WcoEmail::Engine,      :at => '/email/'
+  mount WcoHosting::Engine,      :at => '/hosting'
 
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
