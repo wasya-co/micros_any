3a27458e83b67f0951c54d6ddd9d66a2d3b4a014
mac_a2141 <victor@piousbox.com>
2023-12-20 18:33:15 -0500

    hm

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'application#home'
 
 
-  mount IronWarbler::Engine,      :at => '/trading'
+  mount Iro::Engine,      :at => '/trading'
 
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
