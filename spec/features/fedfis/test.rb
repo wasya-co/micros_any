3ca174a2b6d3d6690fd3aca34ac3fa1264bede11
Victor Pudeyev <piousbox@gmail.com>
2024-01-05 16:41:03 -0600

    hm

--- spec/features/fedfis/test.rb
+++ spec/features/fedfis/test.rb
@@ -1,23 +1,58 @@
 
 require 'capybara/rspec'
 
-Capybara.register_driver :selenium_chrome do |app|
-  # options = Selenium::WebDriver::Chrome::Options.new
-  # options.add_argument '--remote-debugging-port=4444'
-  # Capybara::Selenium::Driver.new(app,
-  #   browser: :chrome,
-  #   options: options )
+Capybara.register_driver :remote_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--window-size=1400,1400")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
 
-  Capybara::Selenium::Driver.new app, browser: :chrome
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    url: "http://#{ENV["SELENIUM_HOST"]}:4444/wd/hub",
+    options: options,
+  )
 end
 
-Capybara.default_driver = :selenium_chrome
+Capybara.register_driver :remote_selenium_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless")
+  options.add_argument("--window-size=1400,1400")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    url: "http://#{ENV["SELENIUM_HOST"]}:4444/wd/hub",
+    options: options,
+  )
+end
+
+Capybara.register_driver :local_selenium do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--window-size=1400,1400")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.register_driver :local_selenium_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless")
+  options.add_argument("--window-size=1400,1400")
+  options.add_argument("--disable-dev-shm-usage")
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.default_driver = :local_selenium_headless
 Capybara.default_max_wait_time = 100 # seconds
 
 RSpec.describe 'fedfis test 1' do
 
   before :all do
-      @driver = Selenium::WebDriver.for(:chrome)
+    # @driver = Selenium::WebDriver.for(:remote_selenium_headless
   end
 
   it 'sanity' do
