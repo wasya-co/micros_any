00be1b3408a5f8f95ad235ca069d7f53b1974adf
mac_a2141 <victor@piousbox.com>
2024-01-10 23:54:27 -0500

    autocommit

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 
-//= link wco_models/application.css
-//= link wco_models/application.js
+//= link wco/application.css
+//= link wco/application.js
 //
 //= link wco_email/application.css
 //= link wco_email/application.js
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,9 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w(
+  wco_models/application.js
+  wco_models/application.css
+
   wco_email/application.js
   wco_email/application.css
 );
