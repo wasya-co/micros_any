7fa77a3bd2a838fd8abb9b7fc0fab0665fcc7e94
mac_a2141 <victor@piousbox.com>
2024-01-10 19:15:51 -0500

    autocommit

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ append :linked_files, "log/production.log",
   "config/initializers/00_s3.rb",
   "config/initializers/05_stripe.rb",
   "config/initializers/08_integrations.rb",
-  "config/initializers/action_mailer.rb",
+  "config/initializers/09_action_mailer.rb",
   "config/master.key",
   "config/mongoid.yml"
 
