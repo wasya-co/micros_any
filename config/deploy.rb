c22925be8fab3cd7af994a0585d8a08fbb8e6a9b
mac_a2141 <victor@piousbox.com>
2024-01-12 20:35:18 -0500

    autocommit

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ namespace :deploy do
       execute "service wco_email_sidekiq restart"
       execute "service wco_email_send_contexts restart"
       execute "service wco_email_run_email_actions restart"
+      execute "service wco_email_churn_stubs restart"
     end
   end
 end
