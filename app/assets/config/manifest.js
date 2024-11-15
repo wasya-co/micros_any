196d361aa00c9eb92190577432146a9bca1673b3
mac_a2141 <victor@piousbox.com>
2024-11-15 17:16:42 -0600

    shuffling rake tasks

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,14 @@
 
+//
 //= link wco/application.css
 //= link wco/application.js
 //
 //= link wco_email/application.css
 //= link wco_email/application.js
-
+//
+//= link wco_hosting/application.css
+//= link wco_hosting/application.js
+//
+//= link iron_warbler/application.css
+//= link iron_warbler/application.js
+//
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,12 +2,20 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w(
-  application_bootstrap.js
+
   application_bootstrap.css
+  application_bootstrap.js
 
-  wco_models/application.js
-  wco_models/application.css
-
-  wco_email/application.js
   wco_email/application.css
+  wco_email/application.js
+
+  iron_warbler/application.css
+  iron_warbler/application.js
+
+  wco_hosting/application.css
+  wco_hosting/application.js
+
+  wco_models/application.css
+  wco_models/application.js
+
 );
